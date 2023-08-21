0b5eb6a75959e19ac148d13304fc5fb57f83ba97
ParkTaewook <parktaewook88@gmail.com>
2023-08-21 23:04:25 +0900

    새로운 프로그래머스 문제

---
+++ test/programmers1_1_8.java
@@ -0,0 +1,33 @@
+package test;
+
+public class programmers1_1_8 {
+//    내적  https://school.programmers.co.kr/learn/courses/30/lessons/70128
+//    문제 설명
+//    길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다. a와 b의 내적을 return 하도록 solution 함수를 완성해주세요.
+//
+//    이때, a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] 입니다. (n은 a, b의 길이)
+
+//                  a	b	result
+//        [1,2,3,4]	[-3,-1,0,2]	3
+//        [-1,0,1]	[1,0,-1]	-2
+
+
+    public static void main(String[] args) {
+
+        int[] a = {1,  2,3,4};
+        int[] b = {-3,-1,0,2};
+        // 0 ,2, 5   5,,4
+
+//        System.out.println(solution(num_lists,3));
+        System.out.println(solution(a, b));
+    }
+
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i=0; a.length > i; i++){
+            answer += a[i]*b[i];
+        }
+
+        return answer;
+    }
+}
---
+++ test/programmers1_1_9.java
@@ -0,0 +1,29 @@
+package test;
+
+public class programmers1_1_9 {
+//      3진법 뒤집기 https://school.programmers.co.kr/learn/courses/30/lessons/68935
+//    문제 설명
+//    자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+//n 	result
+//45	7
+//125	229
+    public static void main(String[] args) {
+
+       int a = 45;
+        // 0 ,2, 5   5,,4
+
+//        System.out.println(solution(num_lists,3));
+        System.out.println(solution(a));
+    }
+
+    public static int solution(int n) {
+        String str = "";
+
+        while (n != 0) {
+            str += n % 3;
+            n /= 3;
+        }
+
+        return Integer.parseInt(str, 3);
+    }
+}
